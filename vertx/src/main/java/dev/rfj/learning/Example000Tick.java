bb47021675d23260f6885a7eacd3107fe2a5e9ff
Roman BRAUNSTINGL <r.braunstingl@celix.at>
2022-08-05 20:43:22 +0200

    add simple vertx example

---
+++ vertx/src/main/java/dev/rfj/learning/Example000Tick.java
@@ -0,0 +1,22 @@
+package dev.rfj.learning;
+
+import io.vertx.core.Vertx;
+
+/**
+ * Simplest vert.x example - start an application and let the event loop call a tick once a second
+ */
+public class Example000Tick {
+    public static void run() {
+        // setup vert.x instance
+        Vertx vertx = Vertx.vertx();
+
+        // note: this would be the place to alter the defaults
+        // Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
+
+        // setting up a timer
+        vertx.setPeriodic(1_000, id -> {
+            // this will run once a second
+            System.out.println("a second has passed!");
+        });
+    }
+}
